fec70b37e7a667746c724dd1163f2e48de3b74b2
LuSiEDa <wltlrtmdgus@naver.com>
2025-10-30 11:05:48 +0900

    init commit

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'products',
+    'orders',
+    # Third-party
+    'rest_framework',
 
 ]
 
--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', HomeView.as_view(), name='home'),
+
 ]
