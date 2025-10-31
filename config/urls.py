fdb9afdcbc6e5d0ee64cf00de135ef69d9d5427b
gmansu1 <gmansu177>
2025-10-31 00:09:26 -0700

    🚧 Feat/orders

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 
-urlpatterns = [ path('admin/', admin.site.urls),
-    path('api/', include('orders.urls')),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', 메인 페이지),
+
+    # users 앱 include
+    path('api/users/', include('users.urls', namespace='users')),
+    # products 앱 include
+    path('api/products/', include('products.urls', namespace='products')),
+
+
+
+
+
+    # 스키마 자동 생성
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    # Swagger UI
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+    # Redoc UI
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
 ]
