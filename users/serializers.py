d06f073278e7401b5912b8bee9420ede967ea641
han-son-03 <rkdgksths11@naver.com>
2025-10-31 15:48:51 +0900

    ✨ Users/view.py 작성중

--- users/serializers.py
+++ users/serializers.py
@@ -8,13 +8,13 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'email', 'username', 'address', 'phone_number', 'created_at', 'updated_at',  'is_active', 'is_admin', 'is_staff')
         read_only_fields = ('id', 'created_at', 'updated_at',  'is_active', 'is_admin', 'is_staff')
 
-    class UserRegisterSerializer(serializers.ModelSerializer):
-        password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-        password2 = serializers.CharField(write_only=True, required=True)  # 비밀번호 확인용
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)  # 비밀번호 확인
 
-        class Meta:
-            model = User
-            fields = ['email', 'username', 'address', 'phone_number', 'password', 'password2']
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'address', 'phone_number', 'password', 'password2']
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import User
+from .serializers import UserSerializer, UserRegisterSerializer
 
-# Create your views here.
+
+
+class UserRegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegisterSerializer
+    permission_classes = [permissions.AllowAny]  # 누구나 접근 가능
+
+# 유저 정보 조회 API
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]  # 로그인 필요
+    lookup_field = 'id'
