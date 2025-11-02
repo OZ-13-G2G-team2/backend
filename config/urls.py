bfb5fbc007a26c18701869a4839119be19cce463
han-son-03 <rkdgksths11@naver.com>
2025-11-02 18:36:21 +0900

    ✨ Fix(config): 주석 추가 및 sellers include

--- config/urls.py
+++ config/urls.py
@@ -8,11 +8,13 @@ urlpatterns = [
     # path('', 메인 페이지)
     # users 앱 include
     path('api/users/', include('users.urls', namespace='users')),
+    #sellers 앱 include
+    path('api/sellers/', include('sellers.urls', namespace='sellers')),
+    #products 앱 include
     path('api/products/', include('products.urls', namespace='products')),
-
     # reviews 앱 include
     path("api/reviews/", include("reviews.urls")),
-
+    # orders 앱 include
     path("api/orders/", include("orders.urls", namespace="orders")),
 
 
