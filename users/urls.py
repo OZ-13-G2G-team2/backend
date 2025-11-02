0e3cce5005e36f32c0e08cf2e2a141ac2f4494f1
han-son-03 <rkdgksths11@naver.com>
2025-11-02 18:48:23 +0900

    🚑️  Critical hotfix.(config): 주석 추가

--- users/urls.py
+++ users/urls.py
@@ -4,9 +4,12 @@ from .views import UserRegisterView, UserLoginView, UserTokenRefreshView, UserDe
 app_name = "users"
 
 urlpatterns = [
+    # user/seller 회원가입
     path('signup/', UserRegisterView.as_view(), name='user-signup'),
     path('signup/seller', SellerRegisterView.as_view(), name='seller-signup'),
+    # 유저 로그인/ 로그아웃
     path('login/', UserLoginView.as_view(), name='token_obtain_pair'),
+    # todo 로그아웃 구현
     path('token/refresh/', UserTokenRefreshView.as_view(), name='token_refresh'),
     path('<int:id>/', UserDetailView.as_view(), name='user-detail'),
 ]
