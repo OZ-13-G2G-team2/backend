de75c44efc5ab6b5fa0a1c47a0e170671dc9e334
han-son-03 <rkdgksths11@naver.com>
2025-11-02 18:51:20 +0900

    ✨ Introduce new features.(sellers): seller CRUD views 추가 추후 축소 계획

--- sellers/views.py
+++ sellers/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from sellers.serializers import SellersSerializer
 
-# Create your views here.
+from sellers.models import Seller
+
+class SellerDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Seller.objects.all()
+    serializer_class = SellersSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    lookup_field = 'id'
