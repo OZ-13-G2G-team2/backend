d3fcf8d589d3e0475bca9ec1213fc8f54214fbfe
han-son-03 <rkdgksths11@naver.com>
2025-11-05 19:31:12 +0900

    🎨 구조 변경에 따른 코드 경로 수정중, rebase하면서 [settings/dev.py] 충돌해결

--- app/users/utils.py
+++ app/users/utils.py
@@ -2,18 +2,18 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.core.mail import send_mail
-from django.conf import settings
+from config.settings import base
 
 
 def send_activation_email(user):
     token = default_token_generator.make_token(user)
     uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
 
-    activation_link = f"{settings.FRONTEND_URL}/activate/{uidb64}/{token}"
+    activation_link = f"{base.FRONTEND_URL}/activate/{uidb64}/{token}"
 
     send_mail(
         subject="이메일 인증 요청",
         message=f"아래 링크를 클릭하여 이메일 인증을 완료해주세요:\n\n{activation_link}",
-        from_email=settings.DEFAULT_FROM_EMAIL,
+        from_email=base.DEFAULT_FROM_EMAIL,
         recipient_list=[user.email],
     )
