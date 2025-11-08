eb05000b00795532aaf533bc8529dc4d572b4fcf
han-son-03 <rkdgksths11@naver.com>
2025-11-08 18:32:02 +0900

    🎨 회원가입 양식 serializers.py 수정, 중복 로직 base로 두어 user,seller 회원가입시 상속

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -29,124 +29,79 @@ class UserSerializer(serializers.ModelSerializer):
             "is_staff",
             "is_superuser",
         )
+#회원가입 공동 로직
+class BaseRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True)
 
+    class Meta:
+        model = User
+        fields = ["email", "username", "address", "phone_number", "password", "password2"]
 
-# email 인증시 임시 회원 생성 시리얼라이저
-class PreSignUpSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    def validate(self, data):
+        if data["password"] != data["password2"]:
+            raise serializers.ValidationError({"password": "비밀번호가 일치하지 않습니다."})
+        if User.objects.filter(email=data["email"]).exists():
+            raise serializers.ValidationError({"email": "이미 사용 중인 이메일입니다."})
+        return data
 
-    def validate_email(self, value):
-        if User.objects.filter(email=value, is_active=True).exists():
-            raise serializers.ValidationError("이미 사용중인 이메일입니다.")
-        return value
+    def create_user(self, validated_data):
+        validated_data.pop("password2")
+        password = validated_data.pop("password")
 
-    def create(self, validated_data):
-        email = validated_data["email"]
-        user, created = User.objects.get_or_create(
-            email=email, defaults={"is_active": False}
-        )
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.is_active = False
+        user.save()
 
         send_activation_email(user)
         return user
 
 
 # 유저 회원가입 시리얼라이저
-class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(
-        write_only=True, required=True, validators=[validate_password]
-    )
-    password2 = serializers.CharField(write_only=True, required=True)  # 비밀번호 확인
-
-    class Meta:
-        model = User
-        fields = [
-            "email",
-            "username",
-            "address",
-            "phone_number",
-            "password",
-            "password2",
-        ]
-
-    def validate(self, data):
-        # 비밀번호 중복 처리
-        if data["password"] != data["password2"]:
-            raise serializers.ValidationError(
-                {"password": "비밀번호가 일치하지 않습니다."}
-            )
-        # 이메일 중복 처리
-        if User.objects.filter(email=data["email"], is_active=True).exists():
-            raise serializers.ValidationError({"email": "이미 사용 중인 이메일입니다."})
-        return data
-
-    def update(self, instance, validated_data):
-        validated_data.pop("password2")
-        password = validated_data.pop("password")
-
-        validated_data.pop("email", None)
-
-        for attr, val in validated_data.items():
-            setattr(instance, attr, val)
-
-        instance.set_password(password)
-        instance.is_active = True
-        instance.save()
-        return instance
+class UserRegisterSerializer(BaseRegisterSerializer):
+    def create(self, validated_data):
+        return self.create_user(validated_data)
 
 
 # 판매자 회원가입
-class SellerRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True)
+class SellerRegisterSerializer(BaseRegisterSerializer):
     business_address = serializers.CharField(write_only=True)
     business_name = serializers.CharField(write_only=True)
     business_number = serializers.CharField(write_only=True)
 
-    class Meta:
-        model = User
-        fields = [
-            "email",
-            "password",
-            "password2",
-            "username",
-            "address",
-            "phone_number",
+    class Meta(BaseRegisterSerializer.Meta):
+        fields = BaseRegisterSerializer.Meta.fields + [
             "business_address",
             "business_name",
             "business_number",
         ]
 
+
     def validate(self, data):
-        if data["password"] != data["password2"]:
-            raise serializers.ValidationError(
-                {"message": "비밀번호가 일치하지 않습니다."}
-            )
+        data = super().validate(data)
+        if Seller.objects.filter(business_number=data["business_number"]).exists():
+            raise serializers.ValidationError({"business_number": "이미 등록된 사업자번호입니다."})
         return data
 
     @transaction.atomic
-    def update(self, instance, validated_data):
+    def create(self, validated_data):
         business_address = validated_data.pop("business_address")
         business_name = validated_data.pop("business_name")
         business_number = validated_data.pop("business_number")
-        validated_data.pop("password2")
 
-        password = validated_data.pop("password")
-        validated_data.pop("email", None)
+        # 공통 유저 생성 로직 재사용
+        user = self.create_user(validated_data)
 
-        for attr, val in validated_data.items():
-            setattr(instance, attr, val)
-
-        instance.set_password(password)
-        instance.is_active = True
-        instance.save()
-
+        # 판매자 생성
         Seller.objects.create(
-            user=instance,
+            user=user,
             business_address=business_address,
             business_name=business_name,
             business_number=business_number,
         )
-        return instance
+
+        return user
 
 
 # 비밀번호 변경
