5a108c2fb0463b9fa7ea3b4f2d7e13291553bfe0
han-son-03 <rkdgksths11@naver.com>
2025-11-08 17:57:20 +0900

    ✨ Users, sellers, 로그인 플로우 와이어 프레임 맞춰 모델 수정

--- app/sellers/models.py
+++ app/sellers/models.py
@@ -11,12 +11,13 @@ class Seller(models.Model):
         related_name="seller_profile",
     )
 
-    business_address = models.CharField(max_length=100, null=True, blank=True)
-    business_name = models.CharField(max_length=225)
-    business_number = models.CharField(max_length=100)
+    business_address = models.CharField(max_length=225, null=True, blank=True)
+    business_name = models.CharField(max_length=225, verbose_name="업체명(상호명)")
+    business_number = models.CharField(max_length=100, verbose_name="사업자등록번호", unique=True)
 
     class Meta:
         db_table = "sellers"
+        verbose_name_plural = "판매자 정보"
 
     def __str__(self):
         return f"{self.business_name} ({self.user.username})"
