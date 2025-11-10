6d1546dc7ec0703baf942f7e9a3b6c4ef7d74e11
LBH\leebh <leebh0806@naver.com>
2025-11-10 15:44:47 +0900

    🚑️ Modified ci_db, config/settings/base.py
    modified ci_db, config/settings/base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+
 
 
 # Application definition
