fc8607e9410025a994de9adf23890696916d09a5
gmansu1 <gmansu177>
2025-11-11 22:39:47 -0800

    수정

--- app/orders/serializers/order_item_serializer.py
+++ app/orders/serializers/order_item_serializer.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from app.orders.models import OrderItem
 
-
 class OrderItemSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source="product.name", read_only=True)
-    subtotal = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
+    product_image = serializers.ImageField(source="product.image", read_only=True)
+    subtotal = serializers.SerializerMethodField()
+    order_status = serializers.CharField(source="order.status", read_only=True)
+    order_date = serializers.DateTimeField(source="order.order_date", read_only=True)
 
     quantity = serializers.IntegerField(
         min_value=1,
@@ -17,6 +19,7 @@ class OrderItemSerializer(serializers.ModelSerializer):
         min_value=0,
         help_text="상품 구매 당시 가격 (음수 불가)",
     )
+
     class Meta:
         model = OrderItem
         fields = [
@@ -24,7 +27,14 @@ class OrderItemSerializer(serializers.ModelSerializer):
             "order",
             "product",
             "product_name",
+            "product_image",
             "quantity",
             "price_at_purchase",
             "subtotal",
+            "order_status",
+            "order_date",
         ]
+
+    def get_subtotal(self, obj):
+        return obj.quantity * obj.price_at_purchase
+
