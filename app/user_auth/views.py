4b4ed1dceea4765673dbc047d2b2992214c0cbba
han-son-03 <rkdgksths11@naver.com>
2025-11-14 11:11:20 +0900

    🎨 배포시 is_active 문제 url로 생각하고 수정 확인 필요

--- app/user_auth/views.py
+++ app/user_auth/views.py
@@ -19,7 +19,7 @@ from app.user_auth.utils import send_activation_email
 
 User = get_user_model()
 
-
+@extend_schema(tags=["이메일 전송"])
 class EmailSendView(APIView):
     permission_classes = [permissions.AllowAny]
 
