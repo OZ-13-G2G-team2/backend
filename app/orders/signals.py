d2ee07ff82cb913a4684dc08e64021544066be42
LuSiEDa <wltlrtmdgus@naver.com>
2025-11-17 19:30:18 +0900

    🎨 Orders 시그널 추가

---
+++ app/orders/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from app.orders.models import OrderItem
+
+
+@receiver(post_save, sender=OrderItem)
+def calculate_total_price_signal(sender, instance, **kwargs):
+    instance.calculate_total_price()
+    instance.save(update_fields=['price_at_purchase'])
